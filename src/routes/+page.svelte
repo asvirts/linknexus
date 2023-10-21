b2b6d3f1dfdb90f0a4d9ae1948cb948e61962880
Andrew Virts <asvirts@outlook.com>
2023-10-21 12:33:14 -0500

    Multi-step Forms

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,2 @@
-<script lang="ts">
-    import type { PageData } from "./$types";
-    import { enhance } from "$app/forms";
-
-    export let data: PageData;
-
-</script>
-
-
-
 <img src="/ln-logo.jpg" alt="Linknexus logo" style="max-width: 600px;">
-<h1>{data.title}</h1>
-<p>{data.text}</p>
-<p>{data.name}</p>
-
-<form method="POST" action="?/rename" use:enhance>
-    <input type="text" name="name">
-    <button type="submit">Submit</button>
-</form>
+<h1 class="text-3xl font-bold underline text-green-500">Linknexus</h1>
