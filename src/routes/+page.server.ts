b72748e4df3d3b635477e21fca2476c73e28c97a
Andrew Virts <asvirts@outlook.com>
2023-10-21 10:58:55 -0500

    svelte actions are awesome

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,23 @@
-import type { PageServerLoad } from './$types';
+import type { PageServerLoad, Actions } from './$types';
 
-export const load = (async () => {
+export const load = (async ({cookies}) => {
+
+    const name = cookies.get('name')
+
     return {
+        name,
         title: 'My data',
         text: 'Hi mom'
-    };
-}) satisfies PageServerLoad;
+    }
+}) satisfies PageServerLoad
+
+export const actions = {
+    default: async ( {request, cookies} ) => {
+        const formData = await request.formData()
+
+        const name = formData.get('name') as string
+
+        cookies.set('name', name)
+
+    }
+} satisfies Actions
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,12 @@
 
 
 
-<img src="/ln-logo.jpg" alt="Linknexus logo">
+<img src="/ln-logo.jpg" alt="Linknexus logo" style="max-width: 600px;">
 <h1>{data.title}</h1>
 <p>{data.text}</p>
+<p>{data.name}</p>
+
+<form method="POST">
+    <input type="text" name="name">
+    <button type="submit">Submit</button>
+</form>
